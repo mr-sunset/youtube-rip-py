7e68b8d7722e44b4a4cd811bb80ec5c6a7d665a1
Jonathan Charette <jacharette@icloud.com>
2026-06-28 15:47:45 -0500

    Added path logic

--- youtube-rip.py
+++ youtube-rip.py
@@ -1,12 +1,14 @@
 import subprocess
 
-# Ask for the YouTube URL
+# Ask for the YouTube URL and save path
 url = input('Enter the share URL, excluding everything after the question mark (https://youtu.be/...):\n> ')
+save_path = input('Enter the complete download path, and finish with the filename:\n> ')
 
 # Run command to rip and download
 result = subprocess.run([
     "yt-dlp", 
     "-x", 
+    "-o", save_path,
     "--audio-format", "mp3", 
     "--extractor-args", "youtube:player_client=default,-android_sdkless", 
     url
